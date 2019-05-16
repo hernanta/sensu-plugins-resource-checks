c0306aa5dfa6d3fa67aad102a8cb8545e9019e11
Hernanta Kusuma Cakrawerdaya <hernantaa@gmail.com>
2019-05-16 18:58:44 +0700

    Update check-memory-usage.sh

--- bin/check-memory-usage.sh
+++ bin/check-memory-usage.sh
@@ -1,8 +1,8 @@
-FREE_DATA=`free -m | grep Mem`
-TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
-FREE=`echo $FREE_DATA | cut -f4 -d' '`
-USED=`expr $TOTAL - $FREE`
-RAM=`echo "scale = 2; $USED/$TOTAL*100" | bc`
+FREE_DATA=$(free -m | grep Mem)
+TOTAL=$(echo $FREE_DATA | cut -f2 -d' ')
+FREE=$(echo $FREE_DATA | cut -f4 -d' ')
+USED=$(expr $TOTAL - $FREE)
+RAM=$(echo "scale = 2; $USED/$TOTAL*100" | bc)
 WARN=0
 if [ $(echo "$RAM > 90" |bc -l) -gt 0 ]; then
     echo "RAM usage is over 90%: ${RAM}%"
