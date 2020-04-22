0d5549693bee20a1ed388e39e769c204b338f86a
KRNLPNC <krnlpnc@pop-os.localdomain>
2020-04-22 12:12:26 +0700

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,10 @@
 - add memory usage script
 - add disk usage script
 - add network usage script
+
+## [1.0.1] - 2019-05-16
+### Added
+- fixed cpu usage script
+- fixed memory usage script
+- fixed disk usage script
+- fixed network usage script
--- bin/check-memory-usage.sh
+++ bin/check-memory-usage.sh
@@ -1,16 +1,23 @@
-FREE_DATA=`free -m | grep Mem`
-TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
-FREE=`echo $FREE_DATA | cut -f4 -d' '`
-USED=`expr $TOTAL - $FREE`
-RAM=`echo "scale = 2; $USED/$TOTAL*100" | bc`
+FREE_DATA=$(free -m | grep Mem)
+TOTAL=$(echo $FREE_DATA | cut -f2 -d' ')
+FREE=$(echo $FREE_DATA | cut -f4 -d' ')
+USED=$(expr $TOTAL - $FREE)
+RAM=$(echo "scale = 2; $USED/$TOTAL*100" | bc)
 WARN=0
+CRIT=0
 if [ $(echo "$RAM > 90" |bc -l) -gt 0 ]; then
     echo "RAM usage is over 90%: ${RAM}%"
+    CRIT=2
+elif [ $(echo "$RAM > 80" |bc -l) -gt 0 ]; then
+    echo "RAM usage is over 80%: ${RAM}%"
     WARN=1
 else
     echo "RAM is normal: ${RAM}%"
 fi
-if [ $(echo "$WARN > 0" |bc -l) -gt 0 ]; then
+
+if [ $(echo "$CRIT > 0" |bc -l) -gt 0 ]; then
+    exit 2
+elif [ $(echo "$WARN > 0" |bc -l) -gt 0 ]; then
     exit 1
 else
     exit 0
