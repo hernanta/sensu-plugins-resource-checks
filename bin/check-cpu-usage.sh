2401c023ec02e6a0a58f1190aaf50fd06e83791c
KRNLPNC <krnlpnc@pop-os.localdomain>
2020-04-22 11:53:32 +0700

    1.0.0

--- bin/check-cpu-usage.sh
+++ bin/check-cpu-usage.sh
@@ -1,19 +1,12 @@
-CPU=$(top -bn 2 -d 0.1 | grep -i "Cpu(s)" | tail -n 1 | gawk '{print $2+$4+$6}')
+CPU=`top -bn 3 -d 0.1 | grep '^%Cpu' | tail -n 1 | gawk '{print $2+$4+$6}'`
 WARN=0
-CRIT=0
 if [ $(echo "$CPU > 90" |bc -l) -gt 0 ]; then
     echo "CPU usage is over 90%: ${CPU}%"
-    CRIT=2
-elif [ $(echo "$CPU > 80" |bc -l) -gt 0 ]; then
-    echo "CPU usage is over 80%: ${CPU}%"
     WARN=1
 else
     echo "CPU is normal: ${CPU}%"
 fi
-
-if [ $(echo "$CRIT > 0" |bc -l) -gt 0 ]; then
-    exit 2
-elif [ $(echo "$WARN > 0" |bc -l) -gt 0 ]; then
+if [ $(echo "$WARN > 0" |bc -l) -gt 0 ]; then
     exit 1
 else
     exit 0
