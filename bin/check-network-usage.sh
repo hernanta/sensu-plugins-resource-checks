e8c113490049b0a0062396dd89453956af4834dd
Hernanta <hernantaa@gmail.com>
2020-08-10 23:00:57 +0700

    Update check-network-usage.sh

--- bin/check-network-usage.sh
+++ bin/check-network-usage.sh
@@ -1,7 +1,7 @@
-in="$(cat /proc/net/dev | egrep 'eth0|ens3' | awk '{print $2}')"
+in="$(cat /proc/net/dev | egrep 'eth0:|ens3' | awk '{print $2}')"
 net_in=$(echo "scale=2; $in/1000000" | bc)
 
-out="$(cat /proc/net/dev | egrep 'eth0|ens3' | awk '{print $10}')"
+out="$(cat /proc/net/dev | egrep 'eth0:|ens3' | awk '{print $10}')"
 net_out=$(echo "scale=2; $out/1000000" | bc)
 
 echo "Bandwidth - Incoming: $net_in MByte"
